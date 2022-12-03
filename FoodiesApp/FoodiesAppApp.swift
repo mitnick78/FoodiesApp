b699c4cb2bbcccae7ba6b51e679bd4e7f71fa738
Christophe <c.milliere>
2022-12-03 22:24:46 +0100

    Initial Commit

---
+++ FoodiesApp/FoodiesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiesAppApp.swift
+//  FoodiesApp
+//
+//  Created by Christophe on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodiesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
